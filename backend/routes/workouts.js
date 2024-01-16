72fd8941d3473be3eb8477e6ed312850ef467704
ameya-6964 <belvalkar7@gmail.com>
2024-01-17 00:39:51 +0530

    Created Controller Functions for Workout and Implemented Them With Respective Routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,37 +1,27 @@
 const express = require("express");
+const {
+  getWorkouts,
+  getWorkout,
+  createWorkout,
+  deleteWorkout,
+  updateWorkout,
+} = require("../controllers/workoutController");
 
 const router = express.Router();
-const Workout = require("../models/workoutModel");
 
 // @route GET /api/workouts/ => Get All Workouts
-router.get("/", (req, res) => {
-  res.json({ message: "GET ALL WORKOUTS" });
-});
+router.get("/", getWorkouts);
 
 // @route GET /api/workouts/:id => Get Single Route
-router.get("/:id", (req, res) => {
-  res.json({ message: "GET SINGLE WORKOUTS" });
-});
+router.get("/:id", getWorkout);
 
 // @route POST /api/workouts/ => Post a New Workout
-router.post("/", async (req, res) => {
-  const { title, load, reps } = req.body;
-  try {
-    const workout = await Workout.create({ title, load, reps });
-    res.status(200).json(workout);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/", createWorkout);
 
 // @route DELETE /api/workouts/:id => Delete Single Workout
-router.delete("/:id", (req, res) => {
-  res.json({ message: "DELETE SINGLE WORKOUT" });
-});
+router.delete("/:id", deleteWorkout);
 
 // @route PUT /api/workouts/:id => Update A Workout
-router.patch("/:id", (req, res) => {
-  res.json({ message: "UPDATE A NEW WORKOUT" });
-});
+router.patch("/:id", updateWorkout);
 
 module.exports = router;
