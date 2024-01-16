4907d6da77e582b1d94199ca74df16c95f90e26c
ameya-6964 <belvalkar7@gmail.com>
2024-01-16 22:09:45 +0530

    Added json Middleware

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const workerRoutes = require("./routes/workouts");
 const port = process.env.PORT || 4000;
 
 //! Logger Middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
