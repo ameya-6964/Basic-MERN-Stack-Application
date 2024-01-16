fec278ac6b6402a03d35052bbfb5e2b40ba9f3c0
ameya-6964 <belvalkar7@gmail.com>
2024-01-16 21:55:31 +0530

    Created First Route Using Express

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,16 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
+//! Logger Middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome To The App" });
+});
+
 app.listen(port, () => {
   console.log(`Listening On Port ${port}`);
 });
