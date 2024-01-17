78398f17fd327d570f5ffe0d8455a86e0757284b
ameya-6964 <belvalkar7@gmail.com>
2024-01-18 01:51:14 +0530

    Added Delete Workout Functionality

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,4 +1,20 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
 const WorkoutDetails = ({ workout }) => {
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
+  const { dispatch } = useWorkoutsContext();
+
+  const handleClick = async () => {
+    const response = await fetch(`${BASE_URL}/api/workouts/${workout._id}`, {
+      method: "DELETE",
+    });
+
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
@@ -11,6 +27,7 @@ const WorkoutDetails = ({ workout }) => {
         {workout.reps}
       </p>
       <p>{workout.createdAt}</p>
+      <span onClick={handleClick}>delete</span>
     </div>
   );
 };
