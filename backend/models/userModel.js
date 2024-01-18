da47e9eeb5858ae9183082a3acde23138365d266
ameya-6964 <belvalkar7@gmail.com>
2024-01-19 01:29:35 +0530

    fixed: exist email Line

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -18,6 +18,11 @@ const userSchema = new Schema({
 
 // static signup method
 userSchema.statics.signup = async function (email, password) {
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error("Email already in use");
+  }
   // validation
   if (!email || !password) {
     throw Error("All fields must be filled");
@@ -27,12 +32,6 @@ userSchema.statics.signup = async function (email, password) {
   }
   if (!validator.isStrongPassword(password)) {
     throw Error("Password not strong enough");
-  }
-
-  const exists = await this.findOne({ email });
-
-  if (exists) {
-    throw Error("Email already in use");
   }
 
   const salt = await bcrypt.genSalt(10);
