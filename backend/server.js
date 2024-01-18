47e24fc10ae8b502f3386e01d39c7450414ad512
ameya-6964 <belvalkar7@gmail.com>
2024-01-19 01:07:06 +0530

    Imported User Routes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 const app = express();
 const workerRoutes = require("./routes/workouts");
+const userRoutes = require("./routes/user");
 const port = process.env.PORT || 4000;
 let connectionString;
 
@@ -25,6 +26,7 @@ app.use((req, res, next) => {
 
 //! Routes
 app.use("/api/workouts", workerRoutes);
+app.use("/api/user", userRoutes);
 
 //Connect To DB
 mongoose
