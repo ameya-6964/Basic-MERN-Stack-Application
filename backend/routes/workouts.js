4ec702eeea0261c0cc965a83d96991062cf9f4c1
ameya-6964 <belvalkar7@gmail.com>
2024-01-19 21:17:15 +0530

    Added Auth Middleware For Protecting Routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -6,22 +6,26 @@ const {
   deleteWorkout,
   updateWorkout,
 } = require("../controllers/workoutController");
+const requireAuth = require("../middleware/requireAuth");
 
 const router = express.Router();
+
+// require auth for all workout routes
+router.use(requireAuth);
 
 // @route GET /api/workouts/ => Get All Workouts
 router.get("/", getWorkouts);
 
-// @route GET /api/workouts/:id => Get Single Route
-router.get("/:id", getWorkout);
-
 // @route POST /api/workouts/ => Post a New Workout
 router.post("/", createWorkout);
+
+// @route GET /api/workouts/:id => Get Single Route
+router.get("/:id", getWorkout);
 
 // @route DELETE /api/workouts/:id => Delete Single Workout
 router.delete("/:id", deleteWorkout);
 
-// @route PUT /api/workouts/:id => Update A Workout
+// @route PATCH /api/workouts/:id => Update A Workout
 router.patch("/:id", updateWorkout);
 
 module.exports = router;
